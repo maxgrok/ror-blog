1bcf665471d3ac2f8b028c864db51af9e1272457
Max Goodman <max.goodman@flatironschool.com>
2018-11-26 09:02:59 -0500

    adding new create and show actions and views

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,12 +5,27 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new(title: params[:title], subtitle: params[:subtitle], body: params[:body], main_image: params[:main_image], thumb_image:params[:thumb_image])
-		@portfolio_item.save
-		@portfolio_item
 	end
 
-	def create
-		@portfolio_item = Portfolio.create(params[:title, :subtitle, :body, :main_image, :thumb_image])
-	end
+  # POST /portfolios
+  # POST /portfolios.json
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item, notice: 'Your portfolio item is now live.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+  
 
 end
