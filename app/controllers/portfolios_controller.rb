79281ad642443fccab28ff786527a92e5d815b11
Max Goodman <max.goodman@flatironschool.com>
2018-11-26 08:41:37 -0500

    adding new action to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,15 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new(title: params[:title], subtitle: params[:subtitle], body: params[:body], main_image: params[:main_image], thumb_image:params[:thumb_image])
+		@portfolio_item.save
+		@portfolio_item
+	end
+
+	def create
+		@portfolio_item = Portfolio.create(params[:title, :subtitle, :body, :main_image, :thumb_image])
+	end
+
 end
